ba3afe5a43f9bb68136b76a17639c9db24116804
Muneeb Mohammed <mumohammed@aligntech.com>
2019-02-28 09:25:29 -0800

    changed class name

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
 
 
 // Getting attribute of element and passing to APP component as Prop
-var el = document.getElementById("container")
+var el = document.getElementById("containerssss")
 var el_attr = el.getAttribute("data-id");
 
 
